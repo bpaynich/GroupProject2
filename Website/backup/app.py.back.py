8f4ed4f814b7f92acf0abae257597587fb888a38
Paynich <bryan.paynich@gmail.com>
2019-03-05 10:11:40 -0700

    reorg commit

--- Website/backup/app.py.back.py
+++ Website/backup/app.py.back.py
@@ -80,15 +80,13 @@ def results_page():
 
     player_results = []
     player_results = engine.execute(f'''
-    SELECT s.Y, s.Player, s.Pos, s.Age, s.Tm, s.G, s.GS, s.MP, s.PER, s.TS, s.PAr, s.FTr, s.STL, s.BLK, s.TOV, 
-	s.PF, s.PTS, a.teamname, a.street, a.city, a.zip, a.latitude, a.longitude, x.venue FROM season_stats_table as s
-	INNER JOIN address_api_table as a ON s.Tm = a.teamacronym
-    INNER JOIN arena_venue_year_table as x on s.Tm = x.abv AND s.Y = x.year
-    WHERE tm = "{team_selected}" and Y = "{year_selected}"
+        select s.Year, s.Player, s.Pos, s.Age, s.Tm, s.G, s.GS, s.MP, s.PER, s.TS, s.PAr, s.FTr, s.STL, s.BLK, s.TOV, s.PF, s.PTS, a.TeamName 
+        from season_stats_table AS s INNER JOIN abv_map_table as a ON s.Tm = a.abbreviations 
+        WHERE tm = "{team_selected}" and Year = "{year_selected}"
         ''').fetchall()
     player_results_df = pd.DataFrame(player_results)
 
-    player_results_df  = player_results_df.rename(columns={player_results_df.columns[0]: "Y", \
+    player_results_df  = player_results_df.rename(columns={player_results_df.columns[0]: "Year", \
                                         player_results_df.columns[1]: "Player", \
                                         player_results_df.columns[2]: "Position", \
                                         player_results_df.columns[3]: "Age", \
@@ -105,16 +103,9 @@ def results_page():
                                         player_results_df.columns[14]: "TOV", \
                                         player_results_df.columns[15]: "PF", \
                                         player_results_df.columns[16]: "PTS", \
-                                        player_results_df.columns[17]: "TeamName", \
-                                        player_results_df.columns[21]: "latitude", \
-                                        player_results_df.columns[22]: "longitude", \
-                                        player_results_df.columns[18]: "street", \
-                                        player_results_df.columns[19]: "city", \
-                                        player_results_df.columns[20]: "zip", \
-                                        player_results_df.columns[23]: "venue"                                            
-                                        })
+                                        player_results_df.columns[17]: "TeamName"})
 
-    year_list       = player_results_df['Y'].tolist()
+    year_list       = player_results_df['Year'].tolist()
     player_list     = player_results_df['Player'].tolist()
     position_list   = player_results_df['Position'].tolist()
     age_list     = player_results_df['Age'].tolist()
@@ -132,38 +123,25 @@ def results_page():
     pf_list     = player_results_df['PF'].tolist()
     pts_list     = player_results_df['PTS'].tolist()
     full_team_name_list     = player_results_df['TeamName'].tolist()
-    arena_venue_ = player_results_df['venue'].tolist()
-    arena_street_ = player_results_df['street'].tolist()
-    arena_city_ = player_results_df['city'].tolist()
-    arena_zip_ = player_results_df['zip'].tolist()
-    lat_coord = player_results_df['latitude'].tolist()
-    long_coord = player_results_df['longitude'].tolist()
-
     players = list(zip(year_list, player_list, position_list, age_list, tm_list, g_list, \
                     gs_list, mp_list, per_list, ts_list, par_list, ftr_list, stl_list, blk_list, tov_list, pf_list, pts_list))
     full_team_name = full_team_name_list[0]
-    lat_c = lat_coord[0]
-    long_c = long_coord[0]
-    arena_street = arena_street_[0]
-    arena_city = arena_city_[0]
-    arena_zip = arena_zip_[0]
-    arena_name = arena_venue_[0]
 
     # Labels for address, team names, coordinates
     #full_team_name = "Phoenix Suns"
-    #lat = 33.4457369
-    #longitude = -112.0712006
-    #arena_name   = "Arena"
-    #arena_street = "401 E Jefferson" 
-    #arena_city   = "Phoenix"
-    arena_state  = ""
-    #arena_zip    = "85201"
+    lat = 33.4457369
+    longitude = -112.0712006
+    arena_name   = "Talking Stick Arena"
+    arena_street = "401 E Jefferson" 
+    arena_city   = "Phoenix"
+    arena_state  = "Arizona"
+    arena_zip    = "85201"
 
     players = list(zip(year_list, player_list, position_list, age_list, tm_list, g_list, \
                     gs_list, mp_list, per_list, ts_list, par_list, ftr_list, stl_list, blk_list, tov_list, pf_list, pts_list))
 
-    return render_template("index.html", full_team_name = full_team_name, team_list = team_list, years_list = years_list, players = players, \
-        arena_name = arena_name, arena_street = arena_street, arena_city = arena_city, arena_state = arena_state, arena_zip = arena_zip, team_year = team_year, lat_c = lat_c, long_c = long_c )
+    return render_template("index.html", full_team_name = full_team_name, team_list = team_list, years_list = years_list, players = players, lat=lat, longitude=longitude, \
+        arena_name = arena_name, arena_street = arena_street, arena_city = arena_city, arena_state = arena_state, arena_zip = arena_zip, team_year = team_year )
 
 if __name__ == "__main__":
     app.run(debug=True)
